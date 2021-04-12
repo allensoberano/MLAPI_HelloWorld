e47b0c192eff8780ddf22a38eaf537748c6b46d6
Allen Soberano <allensoberano@gmail.com>
2021-04-12 11:59:27 -0700

    first commit

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAPI;
+using UnityEngine.UI;
+using MLAPI.Transports.UNET;
+using System;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject menuPanel;
+    public InputField inputField;
+
+    private void Start()
+    {
+        NetworkManager.Singleton.ConnectionApprovalCallback += ApprovalCheck;
+    }
+
+    private void ApprovalCheck(byte[] connectionData, ulong clientID, NetworkManager.ConnectionApprovedDelegate callback)
+    {
+        bool approve = false;
+        //if connection is correct then approve
+        string password = System.Text.Encoding.ASCII.GetString(connectionData);
+        if(password == "mygame")
+        {
+            approve = true;
+        }
+
+        //If approve is true, the connection gets added. If it's false. The client gets disconnected
+        callback(true, null, approve, new Vector3(0,10,0), Quaternion.identity);
+    }
+
+    public void Host()
+    {
+        NetworkManager.Singleton.StartHost();
+        HideMenuPanel();
+    }
+
+    public void Join()
+    {
+        if(inputField.text.Length <= 0)
+        {
+            NetworkManager.Singleton.GetComponent<UNetTransport>().ConnectAddress = "127.0.0.1";
+        } else
+        {
+            NetworkManager.Singleton.GetComponent<UNetTransport>().ConnectAddress = inputField.text;
+        }
+
+        NetworkManager.Singleton.NetworkConfig.ConnectionData = System.Text.Encoding.ASCII.GetBytes("mygame");
+        NetworkManager.Singleton.StartClient();
+        HideMenuPanel();
+    }
+
+    private void HideMenuPanel()
+    {
+        menuPanel.SetActive(false);
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAPI;
+using MLAPI.NetworkVariable;
+using MLAPI.Messaging;
+
+public class PlayerHealth : NetworkBehaviour
+{
+
+   public NetworkVariableFloat health = new NetworkVariableFloat(100f);
+    MeshRenderer[] renderers;
+    CharacterController cc;
+
+    private void Start()
+    {
+        renderers = GetComponentsInChildren<MeshRenderer>();
+        cc = GetComponent<CharacterController>();
+    }
+
+
+    //Running on the server
+    public void TakeDamage(float damage)
+    {
+        health.Value -= damage;
+
+        //check health
+        if(health.Value <= 0)
+        {
+            //respawn
+            Vector3 pos = new Vector3(Random.Range(-10, 10), Random.Range(-10, 10));
+            RespawnClientRpc(pos);
+        }
+    }
+
+    [ClientRpc]
+    void RespawnClientRpc(Vector3 position)
+    {
+        StartCoroutine(Respawn(position));
+
+    }
+
+    IEnumerator Respawn(Vector3 position)
+    {
+        foreach(var renderer in renderers)
+        {
+            renderer.enabled = false;
+        }
+
+        yield return new WaitForSeconds(1f);
+        health.Value = 100f;
+        cc.enabled = false;
+        transform.position = position;
+        cc.enabled = true;
+
+
+        foreach (var renderer in renderers)
+        {
+            renderer.enabled = true;
+        }
+
+    }
+}
